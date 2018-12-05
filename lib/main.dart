97cec3a20a292376a732f8119b604635ebe4c3b0
Kocer Celik <kocerrcelik@gmail.com>
2018-12-05 18:29:40 +0300

    Version 1.0

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'secondPage.dart';
+
+void main() {runApp(MaterialApp(
+  home: MyApp(),
+)); }
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("AppBar"),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.add), onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> SecondPage()));
+            })
+          ],
+        ),
+        body: Center(
+          child: Text("Hello Word"),
+        ),
+      );
+  }
+}
